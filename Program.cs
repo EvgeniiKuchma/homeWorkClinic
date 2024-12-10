4ca797ac2fe6424a8fe841a573340f721da783ed
Евгений Кучма <desferto87@gmail.com>
2024-12-10 21:55:04 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homeWorkClinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int queueSize;
+            int timePerPatient = 10;
+            int minutes = 60;
+            int hours;
+            int minutesRisedue;
+
+            Console.Write($"Pleace enter queue size: ");
+            queueSize = Convert.ToInt32(Console.ReadLine());
+
+            hours = queueSize * timePerPatient / minutes;
+            minutesRisedue = queueSize * timePerPatient % minutes;
+
+            Console.WriteLine($"You must wait in queue {hours} " +
+                $"houres and {minutesRisedue} minutes.");
+        }
+    }
+}
